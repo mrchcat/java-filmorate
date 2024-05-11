81052a9dc07a926ed24f7df61d758c554b4b6155
mrchcat <m-cat@mail.ru>
2024-05-11 18:24:40 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ObjectAlreadyExistsException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ObjectAlreadyExistsException.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.exceptions;
 import lombok.Getter;
 
 @Getter
-public class ObjectAlreadyExistsException extends RuntimeException{
+public class ObjectAlreadyExistsException extends RuntimeException {
     private final Object object;
 
     public ObjectAlreadyExistsException(String message, Object object) {
